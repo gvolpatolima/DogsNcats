751ee60221518a817705327b36a5ba60be4c9792
Volpato <gvolpatolima@gmail.com>
2023-04-26 21:37:02 -0300

    Dataset

---
+++ apps/AI/ai.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+PATH = 'cats_and_dogs'
+
+train_dir = os.path.join(PATH, 'train')
+validation_dir = os.path.join(PATH, 'validation')
+test_dir = os.path.join(PATH, 'test')
