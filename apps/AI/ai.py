1fcc6a191f0b5dd66b4612462faec668e7872f4a
Volpato <gvolpatolima@gmail.com>
2023-04-26 22:06:04 -0300

    File classes correct

--- apps/AI/ai.py
+++ apps/AI/ai.py
@@ -8,13 +8,41 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-PATH = 'cats_and_dogs'
+PATH = os.path.dirname(os.path.abspath('cats_and_dogs/'))
 
 train_dir = os.path.join(PATH, 'train')
 validation_dir = os.path.join(PATH, 'validation')
 test_dir = os.path.join(PATH, 'test')
 
+# Get number of files in each directory. The train and validation directories
+# each have the subdirecories "dogs" and "cats".
+total_train = sum([len(files) for r, d, files in os.walk(train_dir)])
+total_val = sum([len(files) for r, d, files in os.walk(validation_dir)])
+total_test = sum([len(files) for r, d, files in os.walk(test_dir)])
+
+# Variables for pre-processing and training.
 batch_size = 128
 epochs = 15
 IMG_HEIGHT = 150
 IMG_WIDTH = 150
+
+def plotImages(images_arr, probabilities = False):
+    fig, axes = plt.subplots(len(images_arr), 1, figsize=(5,len(images_arr) * 3))
+    if probabilities is False:
+      for img, ax in zip( images_arr, axes):
+          ax.imshow(img)
+          ax.axis('off')
+    else:
+      for img, probability, ax in zip( images_arr, probabilities, axes):
+          ax.imshow(img)
+          ax.axis('off')
+          if probability > 0.5:
+              ax.set_title("%.2f" % (probability*100) + "% dog")
+          else:
+              ax.set_title("%.2f" % ((1-probability)*100) + "% cat")
+    plt.show()
+
+sample_training_images, _ = next(train_data_gen)
+plotImages(sample_training_images[:5])
+
+
