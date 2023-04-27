f32a67080d2058a700c054b53eeeefbb790072c0
Volpato <gvolpatolima@gmail.com>
2023-04-26 22:12:59 -0300

    Trying to add the variables

--- apps/AI/ai.py
+++ apps/AI/ai.py
@@ -8,11 +8,17 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-PATH = os.path.dirname(os.path.abspath('cats_and_dogs/'))
+# PATH = os.path.dirname(os.path.abspath('cats_and_dogs/'))
 
-train_dir = os.path.join(PATH, 'train')
-validation_dir = os.path.join(PATH, 'validation')
-test_dir = os.path.join(PATH, 'test')
+cwd = os.getcwd()
+
+PATH = 'cats_and_dogs'
+
+train_dir = os.path.join(cwd, 'apps', 'AI', 'cats_and_dogs', 'train')
+validation_dir = os.path.join(cwd, 'apps', 'AI', 'cats_and_dogs', 'validation')
+test_dir = os.path.join(cwd, 'apps', 'AI', 'cats_and_dogs', 'test')
+
+print(train_dir)
 
 # Get number of files in each directory. The train and validation directories
 # each have the subdirecories "dogs" and "cats".
@@ -26,23 +32,12 @@ epochs = 15
 IMG_HEIGHT = 150
 IMG_WIDTH = 150
 
-def plotImages(images_arr, probabilities = False):
-    fig, axes = plt.subplots(len(images_arr), 1, figsize=(5,len(images_arr) * 3))
-    if probabilities is False:
-      for img, ax in zip( images_arr, axes):
-          ax.imshow(img)
-          ax.axis('off')
-    else:
-      for img, probability, ax in zip( images_arr, probabilities, axes):
-          ax.imshow(img)
-          ax.axis('off')
-          if probability > 0.5:
-              ax.set_title("%.2f" % (probability*100) + "% dog")
-          else:
-              ax.set_title("%.2f" % ((1-probability)*100) + "% cat")
-    plt.show()
+train_image_generator = ImageDataGenerator(rescale=1./255, rotation_range=20, width_shift_range=0.2, height_shift_range=0.2, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
+validation_image_generator = ImageDataGenerator(rescale=1./255)
+test_image_generator = ImageDataGenerator(rescale=1./255)
 
-sample_training_images, _ = next(train_data_gen)
-plotImages(sample_training_images[:5])
+train_data_gen = train_image_generator.flow_from_directory(train_dir, batch_size=32, target_size=(256, 256), class_mode='binary')
+val_data_gen = validation_image_generator.flow_from_directory(validation_dir, batch_size=32, target_size=(256, 256), class_mode='binary')
+test_data_gen = test_image_generator.flow_from_directory(test_dir, batch_size=32, target_size=(256, 256), class_mode='binary')
 
 
