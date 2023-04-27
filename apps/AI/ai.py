51dd09e44d4d71bf84cb88c8c7c1e05823a021d9
Volpato <gvolpatolima@gmail.com>
2023-04-26 23:35:03 -0300

    AI trained with 71% accuracy

--- apps/AI/ai.py
+++ apps/AI/ai.py
@@ -116,3 +116,25 @@ history = model.fit(
     validation_data=val_data_gen,
     validation_steps=val_data_gen.n // val_data_gen.batch_size
 )
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
