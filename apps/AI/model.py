214daa5732a148975a4bac393c6d198ca48f556a
Volpato <gvolpatolima@gmail.com>
2023-04-29 14:29:17 -0300

    Lot of changes that got lost in the local files

---
+++ apps/AI/model.py
@@ -0,0 +1,95 @@
+import tensorflow as tf
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+import os
+import numpy as np
+
+
+cwd = os.getcwd()
+
+PATH = os.path.join(cwd, 'apps', 'AI', 'cats_and_dogs')
+
+train_dir = os.path.join(cwd, 'apps', 'AI', 'cats_and_dogs', 'train')
+validation_dir = os.path.join(cwd, 'apps', 'AI', 'cats_and_dogs', 'validation')
+test_dir = os.path.join(cwd, 'apps', 'AI', 'cats_and_dogs', 'test')
+
+total_train = sum([len(files) for r, d, files in os.walk(train_dir)])
+total_val = sum([len(files) for r, d, files in os.walk(validation_dir)])
+total_test = sum([len(files) for r, d, files in os.walk(test_dir)])
+
+batch_size = 128
+epochs = 30
+IMG_HEIGHT = 150
+IMG_WIDTH = 150
+
+# Format the images to the correct size so the AI can interpret them
+
+train_image_generator = ImageDataGenerator(rescale=1./255)
+validation_image_generator = ImageDataGenerator(rescale=1./255)
+test_image_generator = ImageDataGenerator(rescale=1./255)
+
+train_data_gen = train_image_generator.flow_from_directory(
+    train_dir,
+    target_size=(IMG_HEIGHT, IMG_WIDTH),
+    batch_size=batch_size,
+    class_mode="binary",
+)
+val_data_gen = validation_image_generator.flow_from_directory(
+    validation_dir,
+    target_size=(IMG_HEIGHT, IMG_WIDTH),
+    batch_size=batch_size,
+    class_mode="binary",
+    seed=42
+)
+test_data_gen = test_image_generator.flow_from_directory(
+    PATH,
+    target_size=(IMG_HEIGHT, IMG_WIDTH),
+    batch_size=1,
+    classes=['test'],
+    shuffle=False,
+)
+
+train_image_generator = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+train_data_gen = train_image_generator.flow_from_directory(batch_size=batch_size,
+    directory=train_dir,
+    target_size=(IMG_HEIGHT, IMG_WIDTH),
+    class_mode='binary')
+
+augmented_images = [train_data_gen[0][0][0] for i in range(5)]
+
+model = Sequential([
+    Conv2D(16, (3,3), activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
+    MaxPooling2D(2, 2),
+    Conv2D(32, (3,3), activation='relu'),
+    MaxPooling2D(2,2),
+    Conv2D(64, (3,3), activation='relu'),
+    MaxPooling2D(2,2),
+    Flatten(),
+    Dense(512, activation='relu'),
+    Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+history = model.fit(
+    train_data_gen,
+    steps_per_epoch=train_data_gen.n // train_data_gen.batch_size,
+    epochs=epochs,
+    validation_data=val_data_gen,
+    validation_steps=val_data_gen.n // val_data_gen.batch_size
+)
+
+model.save('cat_dog_classifier.h5')
