17ecdfcc0f98b195828d3f7f9fc0cbf4720f4637
Volpato <gvolpatolima@gmail.com>
2023-04-30 23:48:37 -0300

    The ai classifies and displays on the website

--- apps/page/tests.py
+++ apps/page/tests.py
@@ -53,7 +53,6 @@ def get_cat():
                 label = 'dog'
             else:
                 label = 'cat'
-
             print(f'The image is classified as a {label}.')
             return JsonResponse({'url': url}), label
         else:
@@ -62,6 +61,59 @@ def get_cat():
     except Exception as e:
         # Handle any exceptions that may occur during the API request
         return JsonResponse({'error': str(e)}, status=500)
+    
+# def get_dog(request):
+#     """
+#     Fetches a random dog image URL from 'https://dog.ceo/api/breeds/image/random' and returns as JSON response.
+#     """
+#     try:
+#         # Send GET request to the API endpoint
+#         response = requests.get('https://dog.ceo/api/breeds/image/random')
+
+#         # Check if the response is successful (status code 200)
+#         if response.status_code == 200:
+#             # Extract the JSON data from the response
+#             data = response.json()
+#             # Extract the 'message' key from the JSON data
+#             message = data['message']
+            
+#             url = message
+
+#             # Load the model
+#             model = tf.keras.models.load_model('cat_dog_classifier.h5')
+
+#             # Request the image from the URL and get the content
+#             response = requests.get(url)
+#             image_content = response.content
+
+#             # Decode the image data and create a PIL image object
+#             image = Image.open(BytesIO(image_content))
+#             # Resize the image to the expected size
+#             image = image.resize((IMG_WIDTH, IMG_HEIGHT))
+#             # Convert the PIL image to a numpy array
+#             image_array = np.array(image)
+#             # Normalize the image data
+#             image_array = image_array / 255.0
+#             # Return the 'url' as JSON response
+#             # Add an extra dimension to the image array to match the input shape of the model
+#             image_array = np.expand_dims(image_array, axis=0)
+
+#             # Use the model to predict the class of the image
+#             prediction = model.predict(image_array)
+
+#             if prediction >= 0.5:
+#                 label = 'dog'
+#             else:
+#                 label = 'cat'
+
+#             return JsonResponse({'url': url}), label
+#         else:
+#             # If the response is unsuccessful, raise an exception
+#             response.raise_for_status()
+#     except Exception as e:
+#         # Handle any exceptions that may occur during the API request
+#         return JsonResponse({'error': str(e)}, status=500)
+    
 
 
 get_cat()
