626a7374b3466f5beef65e024ef7f7a89900c98a
Volpato <gvolpatolima@gmail.com>
2023-04-30 21:23:56 -0300

    It works

--- apps/page/urls.py
+++ apps/page/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, get_dog
+from .views import index, get_dog, get_cat
 
 urlpatterns = [
     path('', index, name='index'),
     path('get_dog/', get_dog, name='get_dog'),
+    path('get_cat/', get_cat, name='get_cat'),
 ]
