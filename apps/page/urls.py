07a746140b1e616362affa7fc5e4059bb3be7641
Gabriel Volpato <gvolpatolima@gmail.com>
2023-05-06 16:12:08 -0300

    url upload image func

--- apps/page/urls.py
+++ apps/page/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import index, get_dog, get_cat
+from .views import index, get_dog, get_cat, upload_image
 
 urlpatterns = [
     path('', index, name='index'),
     path('get_dog/', get_dog, name='get_dog'),
     path('get_cat/', get_cat, name='get_cat'),
-]
+    path('upload_image/', upload_image, name='upload_image'),
+]    
+   
