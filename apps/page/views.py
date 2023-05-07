9a1773664977547706509933ed6305845c10d4e2
Gabriel Volpato <gvolpatolima@gmail.com>
2023-05-06 22:49:37 -0300

    Upload function working

--- apps/page/views.py
+++ apps/page/views.py
@@ -8,6 +8,7 @@ from PIL import Image
 from io import BytesIO
 import tensorflow as tf
 import numpy as np
+from django.http import HttpResponseBadRequest
 
 IMG_HEIGHT = 150
 IMG_WIDTH = 150
@@ -114,40 +115,32 @@ def get_cat(request):
     except Exception as e:
         print(e)
         return JsonResponse({'error': 'Failed to fetch cat image.'})
+    
+    # If the request is not POST or there is an exception, return a default response
+    return HttpResponseBadRequest('Invalid request.')
 
 
 
 def upload_image(request):
-    if request.method == 'POST' and request.FILES['image']:
-        image = request.FILES['image']
-        # process the uploaded image here
-
-        # create a response object
-        response_data = {'status': 'success', 'message': 'Image uploaded successfully'}
-        # Extract the JSON data from the response
-        data = response.json()
-        # Extract the 'url' key from the JSON data
-        message = data['message']
-
+    if request.method == 'POST':
+        print('post')
         # Load the model
         model = tf.keras.models.load_model('cat_dog_classifier.h5')
 
-        # Request the image from the URL and get the content
-        response = requests.get(message)
-        image_content = response.content
-
+        # Get the image from the POST
+        uploaded_file = request.FILES['image']
         # Decode the image data and create a PIL image object
-        image = Image.open(BytesIO(image_content))
+        image = Image.open(BytesIO(uploaded_file.read()))
+        # Convert the image to RGB color space (remove alpha channel)
+        image = image.convert('RGB')
         # Resize the image to the expected size
         image = image.resize((IMG_WIDTH, IMG_HEIGHT))
         # Convert the PIL image to a numpy array
         image_array = np.array(image)
         # Normalize the image data
         image_array = image_array / 255.0
-        # Return the 'message' and the predicted label as a JSON response
         # Add an extra dimension to the image array to match the input shape of the model
         image_array = np.expand_dims(image_array, axis=0)
-
         # Use the model to predict the class of the image
         prediction = model.predict(image_array)
 
@@ -156,10 +149,9 @@ def upload_image(request):
         else:
             label = 'cat'
 
-        return JsonResponse({'message': message, 'label': label})
+        return JsonResponse({'label': label})
     else:
-        response_data = {'status': 'error', 'message': 'Error uploading image'}
-        return JsonResponse(response_data)   
+        return HttpResponseBadRequest('Invalid request.')
 
 def index(request):
     return render(request, 'pages/index.html')
