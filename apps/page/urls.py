0670a272bc9341bca1807c0d0e6241c145737c7a
Gabriel Volpato <gvolpatolima@gmail.com>
2023-05-06 22:49:26 -0300

    Upload Image function added to urls.py

--- apps/page/urls.py
+++ apps/page/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import index, get_dog, get_cat, upload_image
 
+app_name = 'page'
 urlpatterns = [
     path('', index, name='index'),
     path('get_dog/', get_dog, name='get_dog'),
